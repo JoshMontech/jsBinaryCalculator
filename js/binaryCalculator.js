619d10abea5b9acf7b0e85078703fce5a0af668d
Joshua Montgomery <JoshMontgomery@utexas.edu>
2018-01-18 17:56:45 -0600

    fixed a,b bounding

--- js/binaryCalculator.js
+++ js/binaryCalculator.js
@@ -54,7 +54,7 @@ eql.addEventListener('click', function() {
       val[val.length-1] == '+' ||
       val[val.length-1] == '-'
     ) {
-      val = val.substring(0, str.length - 1);
+      val = val.substring(0, val.length - 1);
     }
 
     //clear res
@@ -64,28 +64,91 @@ eql.addEventListener('click', function() {
     var b;
 
     console.log("loopStarted");
-    //step 1, find multiplication and divisions first
+    //step 1, find multiplication and divisions first, left to right
     for (var i = 0; i < val.length; i++) {
 
       if (val[i] == '*') {
         // console.log("* found");
          var operands = getValuesForOp(val, i);
+         var botIndex = i - operands[0].length;
+         var topIndex = operands[1].length + i;
 
          var output = multiply(parseInt(operands[0],2), parseInt(operands[1],2));
 
-         var botIndex = operands[0].length - i;
-         var topIndex = operands[1].length + i;
 
          //find start by getting operand[0].length - i
          //find end by getting operand[1].length + i
          // console.log(operands);
          // console.log("op1 index: " + botIndex, "op2 index: " + topIndex);
 
-        console.log("res after mult",val = val.slice(0, botIndex) + output + val.slice(topIndex+1, val.length));
+        console.log("results after multiplying",val = val.slice(0, botIndex) + output + val.slice(topIndex+1, val.length));
+
+         // console.log("what's left: " + val);
+      }
+
+       else if (val[i] == '/') {
+        // console.log("* found");
+        var operands = getValuesForOp(val, i);
+        var botIndex = i - operands[0].length;
+        var topIndex = operands[1].length + i;
+
+         var output = divide(parseInt(operands[0],2), parseInt(operands[1],2));
+
+
+         //find start by getting operand[0].length - i
+         //find end by getting operand[1].length + i
+         // console.log(operands);
+         // console.log("op1 index: " + botIndex, "op2 index: " + topIndex);
+
+        console.log("results after dividing",val = val.slice(0, botIndex) + output + val.slice(topIndex+1, val.length));
 
          // console.log("what's left: " + val);
       }
     }
+
+    //step 2, do addition and subtraction next
+    for (var i = 0; i < val.length; i++) {
+
+      if (val[i] == '+') {
+        // console.log("* found");
+         var operands = getValuesForOp(val, i);
+         var botIndex = i - operands[0].length;
+         var topIndex = operands[1].length + i;
+
+         var output = add(parseInt(operands[0],2), parseInt(operands[1],2));
+
+
+         //find start by getting operand[0].length - i
+         //find end by getting operand[1].length + i
+         // console.log(operands);
+         // console.log("op1 index: " + botIndex, "op2 index: " + topIndex);
+
+        console.log("results after adding",val = val.slice(0, botIndex) + output + val.slice(topIndex+1, val.length));
+
+         // console.log("what's left: " + val);
+      }
+
+      else if (val[i] == '-') {
+        // console.log("* found");
+         var operands = getValuesForOp(val, i);
+         var botIndex = i - operands[0].length;
+         var topIndex = operands[1].length + i;
+
+         var output = subtract(parseInt(operands[0],2), parseInt(operands[1],2));
+
+
+         //find start by getting operand[0].length - i
+         //find end by getting operand[1].length + i
+         // console.log(operands);
+         // console.log("op1 index: " + botIndex, "op2 index: " + topIndex);
+
+        console.log("total after subtracting",val = val.slice(0, botIndex) + output + val.slice(topIndex+1, val.length));
+
+         // console.log("what's left: " + val);
+      }
+    }
+
+    res.innerHTML = val;
 
 });
 
@@ -98,15 +161,37 @@ function getValuesForOp(arr, i) {
   }
 
   for (var k = i - 1; arr[k] == '0' || arr[k] == '1'; k--) {
-    a += arr[k];
+    a = arr[k] + a;
   }
-
   return [a,b];
+
 
 }
 
 function multiply(a,b) {
-  console.log("input", a, b);
-  console.log("binary: " + (a*b).toString(2))
-  return (a*b).toString(2);
+  var result = a*b;
+  console.log('multiplying ' +a+ ' * ' +b+ ' = ' + result);
+  console.log('binary outcome: ' + result.toString(2))
+  return result.toString(2);
+}
+
+function divide(a,b) {
+  var result = a/b;
+  console.log('dividing ' +a+ ' / ' +b+ ' = ' + result);
+  console.log('binary outcome: ' + result.toString(2))
+  return result.toString(2);
+}
+
+function add(a,b) {
+  var result = Number(a)+Number(b);
+  console.log('adding ' +a+ ' + ' +b+ ' = ' + result);
+  console.log('binary outcome: ' + result.toString(2))
+  return result.toString(2);
+}
+
+function subtract(a,b) {
+  var result = a-b;
+  console.log('subtracting ' +a+ ' - ' +b+ ' = ' + result);
+  console.log('binary outcome: ' + result.toString(2))
+  return result.toString(2);
 }
